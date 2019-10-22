1118d35053d390883e2321bd24700e028448fc5b
nickpape94 <nick-_p@hotmail.com>
2019-10-22 12:40:11 +0100

    working on register option in users/views

--- recipe_project/recipe_project/settings.py
+++ recipe_project/recipe_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'recipe_blog.apps.RecipeBlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
