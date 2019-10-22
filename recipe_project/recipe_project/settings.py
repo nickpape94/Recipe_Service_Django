65366a0c373455d041e46d4b5315c8d327df442d
nickpape94 <nick-_p@hotmail.com>
2019-10-22 16:55:32 +0100

    crispy forms to tidy up registration form

--- recipe_project/recipe_project/settings.py
+++ recipe_project/recipe_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'recipe_blog.apps.RecipeBlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
