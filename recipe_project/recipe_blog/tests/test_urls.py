226de2839e1838e7f4c0ec37214b075fb8e1b8aa
nickpape94 <nick-_p@hotmail.com>
2019-11-01 13:29:29 +0000

    tested all url links besides one

--- recipe_project/recipe_blog/tests/test_urls.py
+++ recipe_project/recipe_blog/tests/test_urls.py
@@ -12,18 +12,30 @@ from recipe_blog.views import (PostListView,
 
 class TestUrls(SimpleTestCase):
 
-    def test_list_url_is_resolved(self):
+    def test_postlist_url_resolves(self):
         url = reverse('recipe-blog-home')
         self.assertEquals(resolve(url).func.view_class, PostListView)
 
-    def test_list_url_is_resolved(self):
+    def test_searchlist_url_resolves(self):
         url = reverse('search')
         self.assertEquals(resolve(url).func.view_class, SearchListView)
 
-    def test_list_url_is_resolved(self):
-        url = reverse('recipe-blog-home')
-        self.assertEquals(resolve(url).func.view_class, PostListView)
+    def test_userpostlist_url_resolves(self):
+        url = reverse('user-posts', args=['some user'])
+        self.assertEquals(resolve(url).func.view_class, UserPostListView)
 
-    def test_list_url_is_resolved(self):
-        url = reverse('search')
-        self.assertEquals(resolve(url).func.view_class, SearchListView)
+    def test_postdetail_url_resolves(self):
+        url = reverse('post-detail', args=[2])
+        self.assertEquals(resolve(url).func.view_class, PostDetailView)
+
+    def test_postcreate_url_resolves(self):
+        url = reverse('post-create')
+        self.assertEquals(resolve(url).func.view_class, PostCreateView)
+
+    def test_postupdate_url_resolves(self):
+        url = reverse('post-update', args=[1])
+        self.assertEquals(resolve(url).func.view_class, PostUpdateView)
+
+    def test_postdelete_url_resolves(self):
+        url = reverse('post-delete', args=[3])
+        self.assertEquals(resolve(url).func.view_class, PostDeleteView)
